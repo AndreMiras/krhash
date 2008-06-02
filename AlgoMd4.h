b1a6f1f515a5145fbce05ffef2e5307b5159da56
andre.miras <andre.miras@944a7194-5f4e-0410-ae2d-71c8ec405a58>
2008-06-02 19:47:45 +0000

    Add support for MD4 algorithm (missed files)

---
+++ AlgoMd4.h
@@ -0,0 +1,87 @@
+/*
+ * TODO:
+ *      - use cryptdll.dll on windows
+ *
+ * READ:
+ *      - http://wiki.qtcentre.org/index.php?title=Cryptography
+ */
+
+
+#ifndef ALGOMD4_H_
+#define ALGOMD4_H_
+
+#include "AbstractAlgo.h"
+
+#include <QByteArray>
+#include <QString>
+
+#include <QTime>
+#include <QCryptographicHash>
+
+#ifdef _WIN32
+// http://source.winehq.org/source/dlls/advapi32/crypt_md4.c
+// http://msdn.microsoft.com/en-us/library/bb432605(VS.85).aspx
+// #include <QCryptographicHash>
+// #include <windows.h>
+//#include <wincrypt.h>
+#else
+#include <openssl/md4.h>
+#endif
+
+#include <iostream>
+
+
+class HashType;
+
+class AlgoMd4 : public AbstractAlgo
+{
+
+public:
+    AlgoMd4();
+
+    /*
+     * Note:    This method has been remade 'cause loosing time in Qt method
+     *          (switch case)
+     * Time:    3848 (-3732) (28790448 tested) for MD4_Init() 
+     *          4597 (-3732) (28790448 tested) for MD4_Update()
+     *          15675 (-3732) (28790448 tested) for MD4_Final()
+     *
+     *
+     */
+    QByteArray hash(const QByteArray & data) const;
+    // inline QByteArray hash(const QByteArray & data) const
+    // { return QCryptographicHash::hash(data, QCryptographicHash::Md4); }
+
+    // unsigned char * AlgoMd4::hashSpeed(const QByteArray & data) const;
+
+    inline QByteArray hashHumanReadable(const QByteArray & data) const
+    { return this->hash(data).toHex(); }
+    // { return data.toHex(); }
+    
+
+    inline QByteArray formatHash(const QByteArray & hash) const
+    { return QByteArray::fromHex(hash); }
+
+    inline QByteArray unFormatHash(const QByteArray & hash) const
+    { return hash.toHex(); }
+
+    /*
+     * TODO:    Use Qt provided method
+     *          imrpove that tests
+     */
+    virtual bool isValid(const QByteArray & hash) const;
+
+
+protected:
+    
+
+private:
+   // QCryptographicHash hashResult;
+    QByteArray hashResult;
+    HashType *ht;
+    // unsigned char * testResult; // convert test
+
+};
+
+#endif /*ALGOMD4_H_*/
+
