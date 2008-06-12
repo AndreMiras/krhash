c327824eafaaf87d07d4db8129819a7a4f93e467
andre.miras <andre.miras@944a7194-5f4e-0410-ae2d-71c8ec405a58>
2008-06-12 23:35:12 +0000

    Performance improvement on dictionary attack

--- trunk/MetaString2.cpp
+++ trunk/MetaString2.cpp
@@ -25,14 +25,11 @@ void MetaString2::init()
     QSettings settings("u54", "KrHash");
     QString dictPath(settings.value("DictPath", "NULL").toString());
     this->setDictionary(dictPath);
-//    std::cout << "DictionaryAttack reading config, DictPath: "<< qPrintable(dictPath) << std::endl;
+    appendAll();
 
 }
 
 
-// FIXME: generatedFrom() instead, so I can remove this when have time
-// FIXME 2: big cleaning cause now using QSettings
-// void MetaString2::setup(const QString & fileName)
 bool MetaString2::setDictionary(const QString & fileName)
 {
     this->file = new QFile(fileName);
@@ -47,43 +44,25 @@ MetaString2::~MetaString2()
 
 void MetaString2::appendAll()
 {
-/*
-    QTextStream inStream(&this->file);
-    std::cout << "MetaString2::AppendingAll()" << std::endl;
     
-    // while (!this->file->atEnd()) 
-    while (!in->atEnd()) 
-    {
-    //      QString line = in.readLine();
-        this->dic->append(this->file->readLine());
-    }
-*/
+    while (!inStream->atEnd()) 
+        dic->append(inStream->readLine(75).toAscii());
+    file->seek(0);
 }
 
+// QByteArray MetaString2::operator++()
 MetaString2 & MetaString2::operator++()
 {
-    // FIXME[cleaning]: disgusting
+    // FIXME[cleaning/performance]: disgusting
     this->clear();
-//    this->append(dic->takeFirst());
-    this->append(this->inStream->readLine(75));
+    this->append(dic->takeFirst());
     return *this;
 }
 
 
 int MetaString2::computeTotal() const
 {
-/*
-    do
-    {
-        line = stream.readLine();
-    } while (!line.isNull());
-*/
-
-    int i=0;
-    while (!inStream->readLine().isNull())
-        ++i;
-    file->seek(0);
-    return i;
+    return dic->size();
 }
 
 void MetaString2::generatedFrom(const QByteArray fileName)
