df2caabf984385dbd81c7156e920d4084fe9a558
andre.miras <andre.miras@944a7194-5f4e-0410-ae2d-71c8ec405a58>
2008-09-30 22:39:11 +0000

    starting to implement the new config brute force dialog

--- ConfigBruteForceWidget.cpp
+++ ConfigBruteForceWidget.cpp
@@ -16,18 +16,45 @@ void ConfigBruteForceWidget::createConfigBruteForceWidget()
     charsetLabel = new QLabel(tr("Charset:"));
     lengthFromLabel = new QLabel(tr("From:"));
     lengthToLabel = new QLabel(tr("To:"));
+
     fromToWidget = new QWidget();
     charsetWidget = new QWidget();
+
+    // TODO: use QSettings to remember settings
+    numericsCheckBox = new QCheckBox(tr("Numerics"));
+    alphaLowerCheckBox = new QCheckBox(tr("Alpha Lower"));
+    alphaUpperCheckBox = new QCheckBox(tr("Alpha Upper"));
+    customCheckBox = new QCheckBox(tr("Custom"));
+
 
 
     charsetLineEdit = new QLineEdit(); // this ?
     charsetLineEdit->setFixedWidth(200);
-    // FIXME: take from default charset
+    // FIXME: take QSettings if set, from default charset if not
     charsetLineEdit->setText("abcdefghijklmnopqrstuvwxyz");
     charsetHBoxLayout = new QHBoxLayout();
     charsetHBoxLayout->addWidget(charsetLabel);
     charsetHBoxLayout->addWidget(charsetLineEdit);
-    charsetWidget->setLayout(charsetHBoxLayout);
+    // charsetWidget->setLayout(charsetHBoxLayout);
+
+
+    connect(numericsCheckBox, SIGNAL(clicked()), this, SLOT(updateCustom()));
+    connect(alphaLowerCheckBox, SIGNAL(clicked()), this, SLOT(updateCustom()));
+    connect(alphaUpperCheckBox, SIGNAL(clicked()), this, SLOT(updateCustom()));
+
+    // TODO: use it to set the focus too
+    // connect(customCheckBox, SIGNAL(clicked()), this, SLOT(updateCustom()));
+    connect(customCheckBox, SIGNAL(toggled(bool)), charsetLineEdit, SLOT(setEnabled(bool)));
+
+    
+    charsetGridLayout = new QGridLayout();
+    charsetGridLayout->addWidget(alphaLowerCheckBox, 0, 0);
+    charsetGridLayout->addWidget(alphaUpperCheckBox, 0, 1);
+    charsetGridLayout->addWidget(numericsCheckBox, 1, 0);
+    charsetGridLayout->addWidget(customCheckBox, 1, 1);
+    charsetGridLayout->addWidget(charsetLineEdit, 2, 0, 1, 2);
+
+    charsetWidget->setLayout(charsetGridLayout);
 
     lengthFromSpinBox = new QSpinBox(); // '()' =? ''
     lengthFromSpinBox->setMinimum(1);
@@ -46,6 +73,9 @@ void ConfigBruteForceWidget::createConfigBruteForceWidget()
     fromToHBoxLayout->addWidget(lengthToLabel);
     fromToHBoxLayout->addWidget(lengthToSpinBox);
     fromToWidget->setLayout(fromToHBoxLayout);
+
+
+    charsetCheckBoxLayout = new QGridLayout();
 
     // FIXME: VerticalLayout instead
     mainVLayout = new QVBoxLayout();
@@ -69,6 +99,26 @@ void ConfigBruteForceWidget::writeSettings()
     settings.setValue("Charset", charsetLineEdit->text());
     settings.setValue("CharsetFrom", lengthFromSpinBox->value());
     settings.setValue("CharsetTo", lengthToSpinBox->value());
+}
+
+
+void ConfigBruteForceWidget::updateCustom()
+{
+    std::cout << "ConfigBruteForceWidget::updateCustom" << std::endl;
+
+    charsetLineEdit->clear();
+
+    if (numericsCheckBox->isChecked())
+        charsetLineEdit->insert("0123456789");
+    if (alphaLowerCheckBox->isChecked())
+        charsetLineEdit->insert("abcdefghijklmnopqrstuvwxyz");
+    if (alphaUpperCheckBox->isChecked())
+        charsetLineEdit->insert("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+}
+
+void ConfigBruteForceWidget::initWidgetStates()
+{
+    QSettings settings("u54", "KrHash");
 }
 
 
