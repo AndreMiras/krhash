9033efba3cb59250c334bbf3eee90de65685900e
andre.miras <andre.miras@944a7194-5f4e-0410-ae2d-71c8ec405a58>
2008-09-30 22:39:11 +0000

    starting to implement the new config brute force dialog

--- branches/config-bruteforce-dial-improv/ConfigBruteForceWidget.h
+++ branches/config-bruteforce-dial-improv/ConfigBruteForceWidget.h
@@ -21,8 +21,10 @@ class QGroupBox;
 class QLabel;
 class QVBoxLayout;
 class QHBoxLayout;
+class QGridLayout;
 class QLineEdit;
 class QSpinBox;
+class QCheckBox;
 
 class ConfigBruteForceWidget : public ConfigAbstractAttackWidget
 {
@@ -41,7 +43,9 @@ private:
 //    QVBoxLayout *vbox;
     QVBoxLayout* mainVLayout;
     QHBoxLayout* fromToHBoxLayout;
-    QHBoxLayout* charsetHBoxLayout;
+    QHBoxLayout* charsetHBoxLayout; // FIXME[cleaning]: not used anymore
+    QGridLayout* charsetGridLayout;
+    QGridLayout* charsetCheckBoxLayout;
     QWidget* fromToWidget;
     QWidget* charsetWidget;
     QLabel* charsetLabel;
@@ -50,8 +54,23 @@ private:
     QLineEdit* charsetLineEdit;
     QSpinBox* lengthFromSpinBox;
     QSpinBox* lengthToSpinBox;
+    QCheckBox* numericsCheckBox;
+    QCheckBox* alphaLowerCheckBox;
+    QCheckBox* alphaUpperCheckBox;
+    QCheckBox* customCheckBox;
     
     void createConfigBruteForceWidget();
+    /*
+     * set all widget states based on arbitrary default and QSettings
+     */
+    void initWidgetStates();
+
+private slots:
+    /*
+     * Update the custom widget whenever a checkbox is hit 
+     *
+     */
+    void updateCustom();
 };
 
 #endif
